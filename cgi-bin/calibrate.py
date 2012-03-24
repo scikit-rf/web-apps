ba269a70b5a26a2d1b16fc95ef46177a01474f27
Alex Arsenovic <arsenovic@virginia.edu>
2012-03-24 16:23:01 -0400

    output preliminarily completed

--- cgi-bin/calibrate.py
+++ cgi-bin/calibrate.py
@@ -17,14 +17,28 @@ form = cgi.FieldStorage()
 
 measured_dir = '../calibration/measured/'
 ideals_dir = '../calibration/ideals/'
+duts_dir = '../calibration/duts/'
+caled_dir = '../calibration/caled/'
 output_dir = '../calibration/output/'
-plot_fmts = ['pdf','png','eps']
+plot_dir = output_dir + 'plots/'
+plot_fmts = ['png','pdf']
+
+component_to_name ={
+	's_db':'Magnitude',
+	's_deg':'Phase',
+	's_smith':'Smith Chart',
+	}
 
 
 n_stds = int(form.getvalue('stdCounter'))
+n_duts = int(form.getvalue('dutCounter'))
+
+
 measured =[]
 ideals = []
+duts = []
 message = ''
+
 for k in range(n_stds):
 	if not(form['measure%i'%(k)].filename and form['ideal%i'%(k)].filename):
 		message ='<script type="text/javascript">alert(\'enter more data\')</script>'
@@ -33,11 +47,16 @@ for k in range(n_stds):
 	fileitem = form['measure%i'%(k)]
 	fn = os.path.basename(fileitem.filename).replace('_','-')
 	open(measured_dir + fn, 'wb').write(fileitem.file.read())
-	measured.append(rf.Network(measured_dir + fn, 'wb'))
+	measured.append(rf.Network(measured_dir + fn))
 	fileitem = form['ideal%i'%(k)]
 	fn = os.path.basename(fileitem.filename).replace('_','-')
 	open(ideals_dir + fn, 'wb').write(fileitem.file.read())
-	ideals.append(rf.Network(ideals_dir + fn, 'wb'))
+	ideals.append(rf.Network(ideals_dir + fn))
+for k in range(n_duts):
+	fileitem = form['dut%i'%(k)]
+	fn = os.path.basename(fileitem.filename).replace('_','-')
+	open(duts_dir + fn, 'wb').write(fileitem.file.read())
+	duts.append(rf.Network(duts_dir + fn))
 
 
 
@@ -45,32 +64,79 @@ cal = rf.Calibration(
 	measured = measured, 
 	ideals = ideals,
 	)
-cal.error_ntwk.write_touchstone( 'ErrorNetwork',dir = output_dir)
-cal.plot_coefs_db()
-pylab.title('Error Coefficients')
-[pylab.savefig(output_dir+'Error Coefficients'+'.'+fmt, format=fmt) for fmt in plot_fmts]
+	
+caled = [cal.apply_cal(k) for k in duts]
 
-message = """
-<center>
-<a href = '%s'>Error Network</a>
-<img width=\'825\' src=\'%s\'></img>
-<br>
-<a href='%s'>png</a>
-<a href='%s'>pdf</a>
-<a href='%s'>eps</a>
-</center>"""\
-%(output_dir+'ErrorNetwork.s2p',
-output_dir+'Error Coefficients'+'.png',
-output_dir+'Error Coefficients'+'.'+ 'pdf',
-output_dir+'Error Coefficients'+'.'+ 'png',
-output_dir+'Error Coefficients'+'.'+ 'eps',)
+message='''
+<div>
+<h2>
+Touchstone Files
+</h2>
+</div>'''
 
-print """\
+for ntwk in caled:
+	fn = ntwk.name+'.s'+str(ntwk.number_of_ports)+'p'
+	message+='''
+	<a href=\'%s\'>%s</a> <br>
+	'''%(caled_dir+fn,fn)
+
+message +='''
+<div>
+<h2>
+Plots 
+</h2>
+</div>'''
+
+
+for component in ['s_db', 's_deg','s_smith']:
+	plot_name = component_to_name[component]
+	message+='''
+	<div>
+	<h3>
+	%s
+	</h3>
+	</div>'''%(plot_name)
+	
+	pylab.figure()
+	pylab.title(plot_name)
+	for ntwk in caled:
+		ntwk.__getattribute__('plot_%s'%component)()
+	
+	 
+	message += '''
+			<div class = "unit">
+			<img width=\'825\' src=\'%s\'></img><br>
+			'''%(plot_dir+plot_name+'.png')
+	
+	for fmt in plot_fmts:
+		fn = plot_dir+plot_name+'.'+fmt
+		pylab.savefig(fn, format=fmt)
+		message += '''<a href = \'%s\'>%s</a>
+		'''%(fn,fmt)
+	message+='''
+		</div>
+		<div text-align="center">
+		'''
+message += '''
+</div>
+'''
+
+print '''\
 Content-Type: text/html\n
 <html>
+
+<head>
+<style>
+	div.unit{
+	text-align:center;
+	padding: 10px;
+	border: 0px solid black;	
+	}
+</style>
+</head>
 <body>
-   <p>%s</p>
+%s
 </body>
 </html>
-""" % message
+''' % message
 
